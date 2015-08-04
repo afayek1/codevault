21351e2fcc27d30514d3358485e9e3568e23faff
Amir Fayek <afayek88@gmail.com>
2015-08-04 11:37:35 -0700

    add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :tags, as: :taggable
+  has_many :comments, as: :commentable
   
   validates :user_id, presence: true
   validates :title, presence: true
