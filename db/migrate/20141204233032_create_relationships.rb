deb359dcc87309dfa1aea94fe0508e8584a4ac6c
Amir Fayek <afayek88@gmail.com>
2015-08-04 00:58:52 -0700

    rearange keyword end to allow migration

--- db/migrate/20141204233032_create_relationships.rb
+++ db/migrate/20141204233032_create_relationships.rb
@@ -4,13 +4,12 @@ class CreateRelationships < ActiveRecord::Migration
   		t.integer	:follower_id
   		t.integer	:followed_id
   		
-      t.timestamps null:false
+      t.timestamps null: false
   	end
-  end
   
   add_index :relationships, :follower_id
   add_index :relationships, :followed_id
-
-  #enforces unqiueness so user can't follow another more than once
+  #enforces uniqueness so user can't follow another more than once
   add_index :relationships, [:follower_id, :followed_id], unique: true
+  end
 end
